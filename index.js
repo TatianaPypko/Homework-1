d0e2968c5ec7b78a06460ef76084f11ba3c3bf4f
Tatiana Pypko <tatiana.pypko@gmail.com>
2022-10-02 19:44:26 +0200

    homework 1

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1-ый способ
+const firstName = prompt("What's your name?", '');
+alert('Hello, ' + firstName + '! How are you?');
+// 2-ой способ
+const lastName = prompt("What's your last name?", '');
+alert(`Welcome, ${firstName} ${lastName}!`);
